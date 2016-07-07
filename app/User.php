c6a1afb8888ec98bf1bb6534f196aa658ac35629
mackhankins <mack.hankins@gmail.com>
2016-07-07 02:06:31 -0500

    shift to 5.2 changes

--- app/User.php
+++ app/User.php
@@ -2,20 +2,13 @@
 
 namespace Msenl;
 
-use Illuminate\Auth\Authenticatable;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Auth\Passwords\CanResetPassword;
-use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
-use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Zizaco\Entrust\Traits\EntrustUserTrait;
 
-use Illuminate\Foundation\Auth\Access\Authorizable;
-use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
-
-class User extends Model implements AuthenticatableContract, AuthorizableContract, CanResetPasswordContract
+class User extends Authenticatable
 {
 
-    use Authenticatable, Authorizable, CanResetPassword, EntrustUserTrait;
+    use EntrustUserTrait;
 
     /**
      * The database table used by the model.
